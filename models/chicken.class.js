14ead6e413741b571588655715cd12fc468b6baf
Ulugbek Kobulov <108155304+KobulovUlugbek@users.noreply.github.com>
2023-06-25 18:15:30 +0200

    added End Boss

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -23,11 +23,9 @@ class Chicken extends MovableObjekt{
 
     animate() {
         this.moveLeft();
+        
         setInterval(() => {
-          let i = this.currentImage % this.IMAGES_WALKING.length;
-          let path = this.IMAGES_WALKING[i];
-          this.img = this.imageCache[path];
-          this.currentImage++;
+          this.playAnimation(this.IMAGES_WALKING);
         }, 160);
       }
 }
